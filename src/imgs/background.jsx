eafce5d943fbf9beaed74a751909df0b49351875
Nguyen Minh Hieu <hieunm@pixelz.com>
2023-03-27 14:43:35 +0700

    add wallpaper

--- src/imgs/background.jsx
+++ src/imgs/background.jsx
@@ -1,13 +1,13 @@
 const BACK_GROUND_IMAGES_PORTRAIT = [
-  "portrait/background1.jpg",
-  "portrait/background2.jpg",
-  "portrait/background3.jpg",
-  "portrait/background4.jpg",
-  "portrait/background5.jpg",
-  "portrait/background6.jpg",
-  "portrait/background7.jpg",
-  "portrait/background8.jpg",
-  "portrait/background9.jpg"
+  "wallpaper/background1.jpg",
+  "wallpaper/background2.jpg",
+  "wallpaper/background3.jpg",
+  "wallpaper/background4.jpg",
+  "wallpaper/background5.jpg",
+  "wallpaper/background6.jpg",
+  "wallpaper/background7.jpg",
+  "wallpaper/background8.jpg",
+  "wallpaper/background9.jpg"
 ]
 
 export { BACK_GROUND_IMAGES_PORTRAIT }
